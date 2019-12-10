27c5fd9167c157937911a807dc9db0fce05f3f81
Raghav Goyal <rgq5943@lainpuma507.springernature.com>
2019-12-10 13:49:16 +0530

    assignment committed

---
+++ ass.js
@@ -0,0 +1,8 @@
+var join = require('./join.js');
+
+var firstName="Raghav";
+var lastName="Goyal";
+
+var fullName = join.joinName(firstName,lastName);
+
+console.log(fullName);
---
+++ join.js
@@ -0,0 +1,6 @@
+function joinName(firstName, lastName)
+{
+	return (firstName+" "+lastName);  // also adding space between firstname and lastname.
+}
+
+module.exports={joinName};
